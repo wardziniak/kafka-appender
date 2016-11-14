9ade060f1a75aee9960aa13c12103cd2335f6b0c
wardziniak <bwardziniak@yahoo.pl>
2016-11-14 21:35:50 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "kafka-appender"
+
+version := "1.0"
+
+scalaVersion := "2.12.0"
+
+libraryDependencies ++= Seq(
+  "ch.qos.logback" % "logback-core" % "1.1.6",
+  "ch.qos.logback" % "logback-classic" % "1.1.6",
+  "org.apache.kafka" % "kafka_2.11" % "0.10.1.0"
+)
---
+++ src/main/scala/com/wardziniak/logback/appender/KafkaAppender.scala
@@ -0,0 +1,35 @@
+package com.wardziniak.logback.appender
+
+import java.util.Properties
+
+import ch.qos.logback.classic.PatternLayout
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.{Layout, UnsynchronizedAppenderBase}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+/**
+  * Created by wardziniak on 13.11.2016.
+  */
+class KafkaAppender extends UnsynchronizedAppenderBase[ILoggingEvent] {
+
+  var props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("acks", "all")
+  props.put("retries", "0")
+  props.put("batch.size", "16384")
+  props.put("linger.ms", "1")
+  props.put("buffer.memory", "33554432")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  var producer: KafkaProducer[String, String] = new KafkaProducer[String, String](props)
+
+  var layout: PatternLayout = new PatternLayout
+
+  override def append(eventObject: ILoggingEvent): Unit = {
+    val message = layout.doLayout(eventObject)
+    val record: ProducerRecord[String, String] = new ProducerRecord[String, String]("test1", message)
+    producer.send(record).get()
+  }
+
+
+}
